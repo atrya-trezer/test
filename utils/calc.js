99bf09c4811b3fb4364c2d4c50dc4442d07a5928
atriya-trezer <fedya.mazin@gmail.com>
2023-01-22 19:04:40 +0300

    commit

--- utils/calc.js
+++ utils/calc.js
@@ -2,7 +2,7 @@
 // код должен делать следующее: 
 // последовательно просить пользователя "Please enter first number: ", "Please enter action (+, -, *, /, ^): ", "Please enter second number: "
 // выполнять соответствующее арифметическое действие, выводить результат и на этом заканчивать работу
-var readlineSync = require('readline-sync');
+let readlineSync = require('readline-sync');
 const mathematics = (firstNumber, action, secondNumber) => {
 	let result = 0;
 	if (action === '+') {
@@ -23,8 +23,23 @@ const mathematics = (firstNumber, action, secondNumber) => {
 	return result;
 }
 
+do {
+	let firstNumber, action, secondNumber;
 
-var firstNumber = Number(readlineSync.question('Please enter first number: '));
-var action = readlineSync.question('Please enter action (+, -, *, /, ^): ');
-var secondNumber = Number(readlineSync.question('Please enter second number: '));
-console.log(mathematics(firstNumber, action, secondNumber));
+	do {
+		firstNumber = Number(readlineSync.question('Please enter first number: '));
+//	console.log(firstNumber, firstNumber === NaN);
+	} while (isNaN(firstNumber));
+
+	const actionVariants = ['+', '-', '*', '/', '^'];
+
+	do {
+		action = readlineSync.question('Please enter action (+, -, *, /, ^): ');
+	} while (actionVariants.includes(action) === false);
+
+	do {
+		secondNumber = Number(readlineSync.question('Please enter second number: '));
+	} while (isNaN(secondNumber));
+
+	console.log(mathematics(firstNumber, action, secondNumber));
+} while (!readlineSync.keyIn('Count more? (y/n) ', {falseValue: ['y']}));
